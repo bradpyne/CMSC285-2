ad8691438851a3e5f5a616e838e10bee96ed3d23
bradpyne <pynebrad@hotmail.com>
2025-10-29 11:36:39 -0400

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+// Michael, Dave, Brad, Sai
+public class Main {
+    public static void main() {
+        System.out.println("Hello World! Initial commit");
+        System.out.println("Brad was here");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+}
