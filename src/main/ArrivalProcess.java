4ff5eb820f98181085d71ee4633a2a65397d2329
Sai Sirigiri <ssirig17@gmail.com>
2025-11-13 20:25:56 -0500

    Update ArrivalProcess class

--- src/main/ArrivalProcess.java
+++ src/main/ArrivalProcess.java
@@ -7,9 +7,29 @@ public class ArrivalProcess {
     private double nextArrivalTime;
     private final Random rng;
 
-    public ArrivalProcess(double startTime, double lambda, Random rng) {
+    // These define where this stream of commuters is coming from / going to
+    private final int startNode;
+    private final int destinationNode;
+
+    // Group size range (how many real people per Commuter)
+    private final int minGroupSize;
+    private final int maxGroupSize;
+
+
+    public ArrivalProcess(double startTime, 
+                          double lambda, 
+                          Random rng, 
+                          int startNode, 
+                          int destinationNode, 
+                          int minGroupSize, 
+                          int maxGroupSize){
+
         this.interArrivalDist = new ExponentialDistribution(lambda);
         this.rng = rng;
+        this.startNode = startNode;
+        this.destinationNode = destinationNode;
+        this.minGroupSize = minGroupSize;
+        this.maxGroupSize = maxGroupSize;
         this.nextArrivalTime = startTime + interArrivalDist.sample();
     }
 
@@ -17,8 +37,13 @@ public class ArrivalProcess {
         return nextArrivalTime;
     }
 
-    public Commuter nextJob(double currentTime) {
-        Commuter c = new Commuter(nextArrivalTime);
+    public Commuter nextCommuter(double currentTime) {
+        // Pick a random group size in [minGroupSize, maxGroupSize]
+        int groupSize = minGroupSize +
+                rng.nextInt(maxGroupSize - minGroupSize + 1);
+        
+        Commuter c = new Commuter(nextArrivalTime, startNode, destinationNode, groupSize);
+
         nextArrivalTime += interArrivalDist.sample();
         return c;
     }
